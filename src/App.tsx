ada97c2fbb2fcf5523aa58d12cd75e5115cf1d12
Pedro Plumer <p.plumer@sap.com>
2021-06-22 09:55:07 -0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-// import Home from './pages/Home';
+import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import './styles/global.scss';
 
 function App() {
   return (
-    // <Home/>
-    <NewRoom/>
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
   );
 }
 
