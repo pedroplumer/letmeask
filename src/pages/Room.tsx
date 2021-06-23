9a16ad63c1d989e5c6894b827a62aaac99c5b478
Pedro Plumer <p.plumer@sap.com>
2021-06-23 09:30:26 -0300

    room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,6 +1,38 @@
+import Button from '../components/Button';
 import React from 'react';
+import logoImg from '../assets/images/logo.svg'
 
-const Room = () => <h1>aaa</h1>
+import '../styles/room.scss'
+
+const Room = () => {
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="letmeask"/>
+                    <div>codigo</div>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala react</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder="O que você gostaria de perguntar?"
+                    />
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
+    )
+}
 
 
 export default Room;
